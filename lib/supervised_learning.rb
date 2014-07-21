63425d1711e559e1e6609e03470458e8271367e5
Michael Imstepf <michael.imstepf@gmail.com>
2014-07-21 16:32:37 +0200

    Adding gradient descent

--- lib/supervised_learning.rb
+++ lib/supervised_learning.rb
@@ -1,5 +1,191 @@
-require "supervised_learning/version"
+require 'supervised_learning/version'
+require 'matrix'
+require 'descriptive_statistics'
 
 module SupervisedLearning
-  # Your code goes here...
+    
+  # This class uses linear regression to make predictions based on a training set.
+  # For datasets of less than 1000 columns, use #predict since this will give the most accurate prediction.
+  # For larger datasets where the #predict method is too slow, use #predict_advanced.
+  # The algorithms in #predict and #predict_advanced were provided by Andrew Ng (Stanford University).
+  # @author Michael Imstepf
+  class LinearRegression
+    # Initializes a LinearRegression object with a training set
+    # @param training_set [Matrix] training_set, each feature/dimension has one column and the last column is the output column (type of value #predict will return)
+    # @raise [ArgumentError] if training_set is not a Matrix and has at least two columns and one row
+    def initialize(training_set)      
+      @training_set = training_set
+      raise ArgumentError, 'input is not a Matrix' unless @training_set.is_a? Matrix
+      raise ArgumentError, 'Matrix must have at least 2 columns and 1 row' unless @training_set.column_size > 1
+
+      @number_of_training_set_columns = @training_set.column_size
+      @number_of_features = @number_of_training_set_columns - 1
+      @number_of_training_examples = @training_set.row_size      
+
+      @output_set = @training_set.column_vectors.last      
+    end
+
+    # Makes prediction using normalization.
+    # This algorithm is the most accurate one but with large
+    # sets (more than 1000 columns) it might take too long to calculate.
+    # @param prediction [Matrix] prediction    
+    def predict(prediction)
+      feature_set = get_feature_set(@training_set, true)      
+
+      validate_prediction_input(prediction)
+            
+      transposed_feature_set = feature_set.transpose # only transpose once for efficiency                  
+      theta = (transposed_feature_set * feature_set).inverse * transposed_feature_set * @output_set
+
+      # add column of ones to prediction
+      prediction = get_feature_set(prediction, true)
+      
+      result_vectorized = prediction * theta
+      result = result_vectorized.to_a.first.to_f
+    end
+
+    # Makes prediction using gradient descent.
+    # This algorithm is requires less computing power
+    # than #predict but is less accurate since it uses approximation.
+    # @param prediction [Matrix] prediction
+    def predict_advanced(prediction, learning_rate = 0.01, iterations = 1000, debug = false) 
+      validate_prediction_input(prediction)
+
+      feature_set = get_feature_set(@training_set, false) 
+      feature_set = normalize_feature_set(feature_set)
+      # add ones to feature set after normalization      
+      feature_set = get_feature_set(feature_set, true)
+
+      # prepare theta column vector with zeros
+      theta = Array.new(@number_of_training_set_columns, 0)
+      theta = Matrix.columns([theta])
+
+      iterations.times do        
+        theta = theta - (learning_rate * (1.0/@number_of_training_examples) * (feature_set * theta - @output_set).transpose * feature_set).transpose
+        if debug
+          puts "Theta: #{theta}"
+          puts "Cost: #{calculate_cost(feature_set, theta)}"
+        end
+      end
+
+      # normalize prediction
+      prediction = normalize_prediction(prediction)
+
+      # add column of ones to prediction
+      prediction = get_feature_set(prediction, true)
+
+      result_vectorized = prediction * theta
+      result = result_vectorized[0,0]
+    end   
+
+    private
+
+    # Returns a feature set without output set (last column of training set)
+    # and optionally adds a leading column of ones to a Matrix.
+    # This column of ones is the first dimension of theta to easily calculate
+    # the output of a function a*1 + b*theta_1 + c*theta_2 etc.    
+    # Ruby's Matrix class has not built-in function for prepending,
+    # hence some manual work is required.
+    # @see http://stackoverflow.com/questions/9710628/how-do-i-add-columns-and-rows-to-a-matrix-in-ruby
+    # @param matrix [Matrix] matrix
+    # @param leading_ones [Boolean] whether to prepend a column of leading ones    
+    # @return [Matrix] matrix
+    def get_feature_set(matrix, leading_ones = false)
+      # get array of columns
+      existing_columns = matrix.column_vectors      
+
+      columns = []
+      columns << Array.new(existing_columns.first.size, 1) if leading_ones           
+      # add remaining columns
+      existing_columns.each_with_index do |column, index|
+        # output column (last column of @training_set) needs to be skipped        
+        # when called from #get_feature_set, matrix includes output column
+        # when called from #prediction, matrix does not inlcude output column
+        break if index + 1 > @number_of_features    
+        columns << column.to_a
+      end
+      
+      Matrix.columns(columns)      
+    end
+
+    # Validates prediction input.
+    # @param prediction [Matrix] prediction
+    # @raise [ArgumentError] if prediction is not a Matrix
+    # @raise [ArgumentError] if prediction does not have the correct number of columns (@training set minus @output_set)
+    # @raise [ArgumentError] if prediction has more than one row
+    def validate_prediction_input(prediction)
+      raise ArgumentError, 'input is not a Matrix' unless prediction.is_a? Matrix
+      raise ArgumentError, 'input has more than one row' if prediction.row_size > 1
+      raise ArgumentError, 'input has wrong number of columns' if prediction.column_size != @number_of_features 
+    end
+
+    # Normalizes feature set for quicker and more reliable calculation.
+    # @param feature_set [Matrix] feature set
+    # @return [Matrix] normalized feature set
+    def normalize_feature_set(feature_set)
+      # create Matrix with mean
+      mean = []
+      feature_set.column_vectors.each do |feature_set_column|
+        # create Matrix of length of training examples for later substraction
+        mean << Array.new(@number_of_training_examples, feature_set_column.mean)
+      end          
+      mean = Matrix.columns(mean)
+      
+      # save for later usage as Matrix and not as Vector
+      @mean = Matrix[mean.row(0)]
+
+      # substract mean from feature set
+      feature_set = feature_set - mean
+
+      # create Matrix with standard deviation
+      standard_deviation = []
+      feature_set.column_vectors.each do |feature_set_column|
+        # create row vector with standard deviation
+        standard_deviation << [feature_set_column.standard_deviation]
+      end            
+      # save for later usage
+      @standard_deviation = Matrix.columns(standard_deviation)
+
+      # Dividing these non-square matrices has to be done manually
+      # (non square matrices have no inverse and can't be divided in Ruby)
+      # iterate through each column    
+      columns = []
+      feature_set.column_vectors.each_with_index do |feature_set_column, index|
+        # manually divide each row within column with standard deviation for that row
+        columns << feature_set_column.to_a.collect { |value| value / @standard_deviation[0,index] }
+      end
+      # reconstruct training set
+      feature_set = Matrix.columns(columns)      
+      feature_set
+    end
+
+    # Normalizes prediction.
+    # @param prediction [Matrix] prediction
+    # @return [Matrix] normalized prediction
+    def normalize_prediction(prediction)
+      # substract mean
+      prediction = prediction - @mean      
+
+      # Dividing these non-square matrices has to be done manually
+      # (non square matrices have no inverse and can't be divided in Ruby)
+      # iterate through each column
+      columns = []
+      prediction.column_vectors.each_with_index do |prediction_column, index|
+        # manually divide each row within column with standard deviation for that row
+        columns << prediction_column / @standard_deviation[0,index]
+      end      
+      # reconstruct prediction
+      prediction = Matrix.columns(columns) 
+    end
+
+    # Calculates cost of current theta.
+    # The closer to 0, the more accurate the prediction will be.
+    # @param feature_set [Matrix] feature set
+    # @param theta [Matrix] theta
+    # @return [Float] cost    
+    def calculate_cost(feature_set, theta)
+      cost_vectorized = 1.0/(2 * @number_of_training_examples) * (feature_set * theta - @output_set).transpose * (feature_set * theta - @output_set)
+      cost_vectorized[0,0]
+    end    
+  end
 end
---
+++ spec/linear_regression_spec.rb
@@ -0,0 +1,118 @@
+require 'spec_helper'
+require 'pry'
+
+describe SupervisedLearning::LinearRegression do
+  training_set_one_feature = Matrix[ [2104,399900], [1600,329900], [2400,369000], [1416,232000], [3000,539900], [1985,299900], [1534,314900], [1427,198999], [1380,212000], [1494,242500], [1940,239999], [2000,347000], [1890,329999], [4478,699900], [1268,259900], [2300,449900], [1320,299900], [1236,199900], [2609,499998], [3031,599000], [1767,252900], [1888,255000], [1604,242900], [1962,259900], [3890,573900], [1100,249900], [1458,464500], [2526,469000], [2200,475000], [2637,299900], [1839,349900], [1000,169900], [2040,314900], [3137,579900], [1811,285900], [1437,249900], [1239,229900], [2132,345000], [4215,549000], [2162,287000], [1664,368500], [2238,329900], [2567,314000], [1200,299000], [852,179900], [1852,299900], [1203,239500] ]
+  program_one_feature = SupervisedLearning::LinearRegression.new(training_set_one_feature)    
+
+  training_set_two_features = Matrix[ [2104,3,399900], [1600,3,329900], [2400,3,369000], [1416,2,232000], [3000,4,539900], [1985,4,299900], [1534,3,314900], [1427,3,198999], [1380,3,212000], [1494,3,242500], [1940,4,239999], [2000,3,347000], [1890,3,329999], [4478,5,699900], [1268,3,259900], [2300,4,449900], [1320,2,299900], [1236,3,199900], [2609,4,499998], [3031,4,599000], [1767,3,252900], [1888,2,255000], [1604,3,242900], [1962,4,259900], [3890,3,573900], [1100,3,249900], [1458,3,464500], [2526,3,469000], [2200,3,475000], [2637,3,299900], [1839,2,349900], [1000,1,169900], [2040,4,314900], [3137,3,579900], [1811,4,285900], [1437,3,249900], [1239,3,229900], [2132,4,345000], [4215,4,549000], [2162,4,287000], [1664,2,368500], [2238,3,329900], [2567,4,314000], [1200,3,299000], [852,2,179900], [1852,4,299900], [1203,3,239500] ]
+  program_two_features = SupervisedLearning::LinearRegression.new(training_set_two_features)
+
+  describe '#initialize' do    
+    context 'when training set is not a matrix' do
+      it 'raises an exception' do
+        expect {SupervisedLearning::LinearRegression.new([1, 2])}.to raise_exception(ArgumentError)
+      end
+    end
+
+    context 'when training set is an empty Matrix' do
+      it 'raises an exception' do
+        expect {SupervisedLearning::LinearRegression.new(Matrix[])}.to raise_exception(ArgumentError)
+      end
+    end
+
+    context 'when training only has one column' do
+      it 'raises an exception' do
+        expect {SupervisedLearning::LinearRegression.new(Matrix[[1]])}.to raise_exception(ArgumentError)
+      end
+    end
+  end
+
+  describe '#predict' do
+    context 'when prediction set is not a matrix' do
+      it 'raises an exception' do
+        expect {program_one_feature.predict([1, 2])}.to raise_exception(ArgumentError)
+      end
+    end
+
+    context 'when prediction has more than one row' do
+      it 'raises an exception' do
+        expect {program_two_features.predict(Matrix[ [1, 2], [3, 4] ])}.to raise_exception(ArgumentError)
+      end
+    end
+
+    context 'when prediction has wrong amount of columns' do
+      context 'when training set has one feature' do
+        it 'raises an exception' do
+          expect {program_one_feature.predict(Matrix[ [1, 2] ])}.to raise_exception(ArgumentError)
+          expect {program_one_feature.predict(Matrix[ ])}.to raise_exception(ArgumentError)           
+        end        
+      end
+
+      context 'when training set has two features' do
+        it 'raises an exception' do
+          expect {program_two_features.predict(Matrix[ [1, 2, 3] ])}.to raise_exception(ArgumentError)
+          expect {program_two_features.predict(Matrix[ [1] ])}.to raise_exception(ArgumentError)                 
+        end        
+      end      
+    end
+
+    context 'when prediction has correct amount of columns' do
+      context 'when training set has one feature' do
+        it 'returns correct prediction' do
+          expect(program_one_feature.predict(Matrix[ [1650] ]).to_i).to eq 293237
+        end        
+      end
+
+      context 'when training set has two features' do
+        it 'returns correct prediction' do
+          expect(program_two_features.predict(Matrix[ [1650, 3] ]).to_i).to eq 293081
+        end        
+      end            
+    end              
+  end
+
+  describe '#predict_advanced' do
+    context 'when prediction set is not a matrix' do
+      it 'raises an exception' do
+        expect {program_one_feature.predict_advanced([1, 2])}.to raise_exception(ArgumentError)
+      end
+    end
+
+    context 'when prediction has more than one row' do
+      it 'raises an exception' do
+        expect {program_two_features.predict_advanced(Matrix[ [1, 2], [3, 4] ])}.to raise_exception(ArgumentError)
+      end
+    end
+
+    context 'when prediction has wrong amount of columns' do
+      context 'when training set has one feature' do
+        it 'raises an exception' do
+          expect {program_one_feature.predict_advanced(Matrix[ [1, 2] ])}.to raise_exception(ArgumentError)
+          expect {program_one_feature.predict_advanced(Matrix[ ])}.to raise_exception(ArgumentError)           
+        end        
+      end
+
+      context 'when training set has two features' do
+        it 'raises an exception' do
+          expect {program_two_features.predict_advanced(Matrix[ [1, 2, 3] ])}.to raise_exception(ArgumentError)
+          expect {program_two_features.predict_advanced(Matrix[ [1] ])}.to raise_exception(ArgumentError)                 
+        end        
+      end      
+    end
+
+    context 'when prediction has correct amount of columns' do
+      context 'when training set has one feature' do
+        it 'returns correct prediction' do
+          expect(program_one_feature.predict_advanced(Matrix[ [1650] ], 0.1, 600, false).to_i).to be_within(200).of(293237)
+        end        
+      end
+
+      context 'when training set has two features' do
+        it 'returns correct prediction' do
+          expect(program_two_features.predict_advanced(Matrix[ [1650, 3] ], 0.1, 600, false).to_i).to be_within(200).of(293237)
+        end        
+      end            
+    end              
+  end      
+end
